124f9b78ceb4d6d0ddc4863303b61ea9d0c48d37
zivadmn8866 <zivadmn@gmail.com>
2025-11-26 14:04:42 +0530

    vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 
 export default defineConfig({
   root: '.',
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src')
+      '@': '/src'
     }
   },
   server: {
